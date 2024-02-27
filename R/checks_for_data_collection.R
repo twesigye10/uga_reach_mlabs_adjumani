49127f032c1a7be9e834d9fd5abccb20e1e20245
Amos Nyarwaya <amos.nyarwaya@gmail.com>
2024-02-27 19:47:29 +0300

    initial commit

---
+++ R/checks_for_data_collection.R
@@ -0,0 +1,146 @@
+library(tidyverse)
+library(glue)
+library(cleaningtools)
+library(httr)
+library(supporteR)
+
+source("R/support_functions.R")
+source("support_files/credentials.R")
+
+
+# read data ---------------------------------------------------------------
+
+loc_data <- "inputs/UGA2401_Adjumani_ECHO_data.xlsx"
+df_tool_data <- readxl::read_excel(loc_data)
+
+# tool
+
+loc_tool <- "inputs/UGA2401_Adjumani_ECHO_tool.xlsx"
+df_survey <- readxl::read_excel(loc_tool, sheet = "survey")
+df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
+
+# download audit files folder
+
+download_audit_files(df= df_tool_data,
+                     uuid_column = "_uuid",
+                     audit_dir = "inputs/audit_files",
+                     usr = user_acc,
+                     pass = user_pss)
+
+# zip audit files folder
+
+if (dir.exists("inputs/audit_files")) {
+    zip::zip(zipfile = "inputs/audit_files.zip",
+             # files = "inputs/audit_files",
+             files = list.dirs(path = "inputs/audit_files/", full.names = TRUE, recursive = FALSE),
+             mode = "cherry-pick")
+
+}
+
+
+# check pii ---------------------------------------------------------------
+
+pii_cols <- c("telephone", "contact", "name", "gps", "latitude", "longitude", "contact", "geopoint")
+pii_from_data <- cleaningtools::check_pii(dataset = df_tool_data, element_name = "checked_dataset", uuid_column = "_uuid")
+pii_from_data$potential_PII
+
+
+# duration ----------------------------------------------------------------
+# read audit file
+
+audit_list_data <- cleaningtools::create_audit_list(audit_zip_path = "inputs/audit_files.zip")
+
+# add duration from audit
+df_tool_data_with_audit_time <- cleaningtools::add_duration_from_audit(df_tool_data, uuid_column = "_uuid", audit_list = audit_list_data)
+
+
+
+# Exporting the flags in excel --------------------------------------------
+
+# outliers columns not to check
+outlier_cols_not_4_checking <- df_tool_data %>% 
+    select(matches("geopoint|gps|_index|_submit|submission|_sample_|^_id$")) %>% 
+    colnames()
+
+# create_combined_log()
+list_log_refugee <- df_tool_data_with_audit_time %>%
+    check_pii(uuid_column = "_uuid") %>%
+    check_duration(column_to_check = "duration_audit_sum_all_minutes",
+                   uuid_column = "_uuid",
+                   log_name = "duration_log",
+                   lower_bound = 20,
+                   higher_bound = 120) %>% 
+    check_outliers(uuid_column = "_uuid", sm_separator = "/",
+                   strongness_factor = 3, columns_not_to_check = outlier_cols_not_4_checking) %>% 
+    check_soft_duplicates(kobo_survey = df_survey_refugee,
+                          uuid_column = "_uuid",
+                          idnk_value = "dk",
+                          sm_separator = "/",
+                          log_name = "soft_duplicate_log",
+                          threshold = 7,
+                          return_all_results = FALSE) %>%
+    check_value(uuid_column = "_uuid", values_to_look = c(666, 99, 999, 9999, 98, 88, 888, 8888))
+
+# others checks
+df_other_checks_refugee <- cts_format_other_specify(input_tool_data = df_tool_data_refugee, 
+                                                    input_uuid_col = "_uuid", 
+                                                    input_survey = df_survey_refugee, 
+                                                    input_choices = df_choices_refugee)
+# add other checks to the list
+list_log_refugee$other_log <- df_other_checks
+
+
+# silhouette analysis -----------------------------------------------------
+# NOTE: the column for "col_admin" is kept in the data
+
+omit_cols_sil <- c("start", "end", "today", "duration", "duration_minutes",
+                   "deviceid", "audit", "audit_URL", "instance_name", "end_survey",
+                   "geopoint", "_geopoint_latitude", "_geopoint_altitude", "_geopoint_precision", "_id" ,"_submission_time","_validation_status","_notes","_status","_submitted_by","_tags","_index")
+
+data_similartiy_sil <- df_tool_data_refugee %>% 
+    select(- any_of(omit_cols_sil), - matches("_note$|^note_"))
+
+df_sil_data <- calculateEnumeratorSimilarity(data = data_similartiy_sil,
+                                             input_df_survey = df_survey, 
+                                             col_enum = "enumerator_id",
+                                             col_admin = "interview_cell") %>% 
+    mutate(si2= abs(si))
+
+df_sil_processed <- df_sil_data[order(df_sil_data$`si2`, decreasing = TRUE),!colnames(df_sil_data)%in%"si2"] %>%  
+    filter(si > 0.6) %>% 
+    mutate(i.check.uuid = "all",
+           i.check.question = NA_character_,
+           i.check.issue = paste("Potential similar responses for enumerator. si: ",si)) %>% 
+    batch_select_rename()
+
+# add other checks to the list
+list_log_refugee$enum_similarity <- df_sil_processed
+
+
+# combine the checks ------------------------------------------------------
+
+df_combined_log_refugee <- create_combined_log(dataset_name = "checked_dataset", list_of_log = list_log_refugee)
+
+# add_info_to_cleaning_log()
+add_with_info_refugee <- add_info_to_cleaning_log(list_of_log = df_combined_log_refugee,
+                                                  dataset = "checked_dataset",
+                                                  cleaning_log = "cleaning_log",
+                                                  dataset_uuid_column = "_uuid",
+                                                  cleaning_log_uuid_column = "uuid",
+                                                  information_to_add = c("enumerator_id", "today", "interview_cell")
+)
+
+# create_xlsx_cleaning_log()
+add_with_info_refugee |>
+    create_xlsx_cleaning_log(
+        kobo_survey = df_survey_refugee,
+        kobo_choices = df_choices_refugee,
+        use_dropdown = TRUE,
+        output_path = paste0("outputs/", butteR::date_file_prefix(), 
+                             "_combined_checks_aba_mbarara_refugee.xlsx")
+    )
+
+
+
+
+
---
+++ R/support_functions.R
@@ -0,0 +1,153 @@
+# download audit files ----------------------------------------------------
+
+download_audit_files <- function(df, uuid_column = "_uuid", audit_dir, usr, pass){
+    if (!"httr" %in% installed.packages()) 
+        stop("The package is httr is required!")
+    if (is.na(audit_dir) || audit_dir == "") 
+        stop("The path for storing audit files can't be empty!")
+    if (is.na(usr) || usr == "") 
+        stop("Username can't be empty!")
+    if (is.na(pass) || pass == "") 
+        stop("Password can't be empty!")
+    
+    # checking if the output directory is already available
+    if (!dir.exists(audit_dir)) {
+        dir.create(audit_dir)
+        if (dir.exists(audit_dir)) {
+            cat("Attention: The audit file directory was created in", audit_dir,"\n")
+        }
+    }
+    
+    # checking if creating output directory was successful
+    if (!dir.exists(audit_dir))
+        stop("download_audit_fils was not able to create the output directory!")
+    # checking if uuid column exists in data set
+    if (!uuid_column %in% names(df))
+        stop("The column ", uuid_column, " is not available in data set.")
+    # checking if column audit_URL exists in data set
+    if (!uuid_column %in% names(df))
+        stop("The column ", uuid_column, " is not available in data set.")
+    if (!"audit_URL" %in% names(df))
+        stop("Error: the column audit_URL is not available in data set.")
+    
+    # getting the list of uuids that are already downloaded
+    available_audits <- dir(audit_dir)
+    
+    # excluding uuids that their audit files are already downloaded
+    df <- df[!df[[uuid_column]] %in% available_audits,]
+    
+    audits_endpoint_link <- df[["audit_URL"]]
+    names(audits_endpoint_link) <- df[[uuid_column]]
+    audits_endpoint_link <- na.omit(audits_endpoint_link)
+    
+    if (length(audits_endpoint_link) > 0) {
+        # iterating over each audit endpoint from data
+        for (i in 1:length(audits_endpoint_link)) {
+            uuid = names(audits_endpoint_link[i])
+            endpoint_link_i <- audits_endpoint_link[i]
+            cat("Downloading audit file for", uuid, "\n")
+            
+            # requesting data
+            audit_file <- content(GET(endpoint_link_i,
+                                      authenticate(usr, pass),
+                                      timeout(1000),
+                                      progress()), "text", encoding = "UTF-8")
+            
+            if (!is.na(audit_file)) {
+                if (length(audit_file) > 2) {
+                    dir.create(paste0(audit_dir, "/", uuid), showWarnings = F)
+                    write.csv(audit_file, paste0(audit_dir, "/", uuid, "/audit.csv"), row.names = F)
+                }else if(!audit_file == "Attachment not found"){
+                    if (grepl("[eventnodestartend]", audit_file)) {
+                        dir.create(paste0(audit_dir, "/", uuid), showWarnings = F)
+                        write.table(audit_file, paste0(audit_dir, "/", uuid, "/audit.csv"), row.names = F, col.names = FALSE, quote = F)
+                    } else{
+                        cat("Error: Downloading audit was unsucessful!\n")
+                    }
+                }
+            } else{
+                cat("Error: Downloading audit was unsucessful!\n")
+            }
+        }
+    } else{
+        cat("Attention: All audit files for given data set is downloaded!")
+    }
+}
+
+# silhouette analysis based on gower distance between surveys -------------
+
+# silhouette analysis based on gower distance between surveys
+# METHOD: check for anomalies using the silhouette function. We assume the dataset is clustered using the 
+# enumerator IDs as the cluster IDs and we calculate the silhouette for this clustering scenario. A 
+# silhouette value close to 1 indicates that the entries of the cluster are very similar to each other and 
+# very dissimilar from entries of other clusters. Thus, we need to raise a flag if the silhouette value gets 
+# close to 1 for any of the clusters/enumerators.
+# https://en.wikipedia.org/wiki/Silhouette_(clustering)
+# https://dpmartin42.github.io/posts/r/cluster-mixed-types
+# https://medium.com/@rumman1988/clustering-categorical-and-numerical-datatype-using-gower-distance-ab89b3aa90d9
+
+calculateEnumeratorSimilarity <- function(data, input_df_survey, col_enum, col_admin){
+    
+    # helper function
+    convertColTypes <- function(data, input_df_survey){
+        # select_multiple: numeric or factor?
+        col.types <- data.frame(column=colnames(data)) %>% 
+            left_join(select(input_df_survey, name, type), by=c("column"="name")) %>% 
+            mutate(type.edited = case_when(type %in% c("integer", "decimal", "calculate") ~ "numeric",
+                                           str_starts(type, "select_") ~ "factor",
+                                           str_detect(column, "/") ~ "factor",
+                                           TRUE ~ "text"))
+        
+        cols <- col.types[col.types$type.edited=="numeric", "column"]
+        data[,cols] <- lapply(data[,cols], as.numeric)
+        cols <- col.types[col.types$type.edited=="text", "column"]
+        data[,cols] <- lapply(data[,cols], as.character)
+        cols <- col.types[col.types$type.edited=="factor", "column"]
+        data[,cols] <- lapply(data[,cols], as.factor)
+        
+        return(data)
+    }
+    
+    # convert columns using the tool
+    data <- convertColTypes(data, input_df_survey)
+    # keep only relevant columns
+    cols <- data.frame(column=colnames(data)) %>% 
+        left_join(select(input_df_survey, name, type), by=c("column"="name")) %>% 
+        filter(!(type %in% c("_uuid", "enumerator_id")) &
+                   !str_starts(column, "_other$") &
+                   !str_detect(column, "_specify$"))
+    # convert character columns to factor and add enum.id
+    data <- data[, all_of(cols$column)] %>% 
+        mutate_if(is.character, factor) %>% 
+        arrange(!!sym(col_enum)) %>%
+        mutate(enum.id=as.numeric(!!sym(col_enum)), .after=!!sym(col_enum))
+    
+    # calculate similarity (for enumerators who completed at least 5 surveys)
+    res <- data %>% split(data[[col_admin]]) %>% 
+        lapply(function(gov){
+            df <- gov %>% 
+                group_by(enum.id) %>% 
+                mutate(n=n()) %>% 
+                filter(n>=5) %>% 
+                ungroup() %>% 
+                select_if(function(x) any(!is.na(x)))
+            
+            if (length(unique(df$enum.id)) > 1){
+                # calculate gower distance
+                gower_dist <- daisy(select(df, -c(!!sym(col_enum), enum.id)), 
+                                    metric = "gower", warnBin = F, warnAsym = F, warnConst = F)
+                # gower_mat <- as.matrix(gower_dist)
+                # calculate silhouette
+                si <- silhouette(df$enum.id, gower_dist)
+                res.si <- summary(si)
+                # create output
+                r <- data.frame(enum.id=as.numeric(names(res.si$clus.avg.widths)), si=res.si$clus.avg.widths) %>% 
+                    left_join(distinct(select(df, !!sym(col_admin), !!sym(col_enum), enum.id)), by="enum.id") %>% 
+                    left_join(group_by(df, enum.id) %>% summarise(num.surveys=n(), .groups="drop_last"), by="enum.id") %>% 
+                    select(!!sym(col_admin), !!sym(col_enum), num.surveys, si) %>% 
+                    arrange(-si)
+                return(r)
+            }
+        })
+    do.call(rbind, res)
+}
