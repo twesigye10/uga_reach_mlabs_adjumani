e42bc481a91b856d174c51bc4e4bae40ea5d7ca1
Amos Nyarwaya <amos.nyarwaya@gmail.com>
2024-03-06 15:41:03 +0300

    removed threshold

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -184,7 +184,7 @@ df_sil_data <- calculateEnumeratorSimilarity(data = data_similartiy_sil,
     mutate(si2= abs(si))
 
 df_sil_processed <- df_sil_data[order(df_sil_data$`si2`, decreasing = TRUE),!colnames(df_sil_data)%in%"si2"] %>%  
-    filter(si > 0.6) %>% 
+    # filter(si > 0.6) %>% 
     mutate(i.check.uuid = "all",
            i.check.question = NA_character_,
            i.check.issue = paste("Potential similar responses for enumerator. si: ",si)) %>% 
