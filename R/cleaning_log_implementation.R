61777c247a46abf50792a43498f0f759a363f147
Anthony Twesigye <twesigye10@gmail.com>
2024-04-05 10:42:33 +0300

    adapting updating function for sm parents

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -3,6 +3,8 @@ library(glue)
 library(cleaningtools)
 library(httr)
 
+source("R/support_functions.R")
+
 loc_data <- "inputs/UGA2401_Adjumani_ECHO_data.xlsx"
 
 df_tool_data <- readxl::read_excel(loc_data) %>% 
@@ -33,52 +35,9 @@ cols_to_remove <- c("audit", "audit_URL", "pt_num_msg", "pt_num_validation_messa
 
 # updating the dataset with new columns -----------------------------------
 
-# gather choice options based on unique choices list
-df_grouped_choices<- df_choices %>%
-    group_by(list_name) %>%
-    summarise(choice_options = paste(name, collapse = " : "))
-
-# get new name and choice pairs to add to the choices sheet
-new_vars_sm <- df_filled_cl %>%
-    filter(str_detect(string = question, pattern = "\\w+\\/+\\w+")) %>%
-    filter(!str_detect(string = question, pattern = "other$"), change_type %in% c("change_response")) %>%
-    mutate(int.new_value = str_replace_all(string = question, pattern = "\\w+\\/", replacement = ""),
-           int.question = str_replace_all(string = question, pattern = "\\/+\\w+", replacement = "")) %>% 
-    left_join(df_survey, by = c("int.question" = "name")) %>%
-    filter(str_detect(string = type, pattern = "select_one|select one|select_multiple|select multiple")) %>%
-    separate(col = type, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop") %>%
-    left_join(df_grouped_choices, by = "list_name") %>%
-    filter(!str_detect(string = choice_options, pattern = int.new_value)) %>%
-    select(question) %>%
-    # distinct() %>% # to make sure there are no duplicates
-    # arrange(question) %>% 
-    group_by(question) %>% 
-    summarise(n = n())
-
-# write_csv(new_vars_sm, "outputs/test_new_choices.csv")
-
-# handle when a question had not been answered
-df_add_columns_to_data <- df_tool_data %>% 
-    butteR:::mutate_batch(nm = new_vars_sm$question, value = NA_character_ ) # %>% 
-
-# parent questions for select multiple
-col_changes_parent_vars_sm <- new_vars_sm %>% 
-    mutate(question = str_replace_all(string = question, pattern = "/.+", replacement = "")) %>% 
-    pull(question) %>% 
-    unique()
-
-df_handle_sm_data <- df_add_columns_to_data
-
-for (cur_sm_col in col_changes_parent_vars_sm) {
-    df_updated_data <- df_handle_sm_data %>% 
-        mutate(
-            across(contains(paste0(cur_sm_col, "/")), .fns = ~ifelse(!is.na(!!sym(cur_sm_col)) & is.na(.) , 0, .)),
-            across(contains(paste0(cur_sm_col, "/")), .fns = ~ifelse(is.na(!!sym(cur_sm_col)), NA_integer_, .))
-        )
-    df_handle_sm_data <- df_updated_data
-}
-
-df_data_with_added_cols <- df_handle_sm_data
+df_data_with_added_cols <- cts_add_new_sm_choices_to_data(input_df_tool_data = df_tool_data,
+                                                          input_df_filled_cl = df_filled_cl, 
+                                                          input_df_choices = df_choices)
 
 
 
