7caa4a73799c65b5c8d53b9173ac9f7006038043
Anthony Twesigye <twesigye10@gmail.com>
2024-04-12 15:30:42 +0300

    correct analysis column

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -70,8 +70,10 @@ df_analysis_host <- analysistools::create_analysis(design = host_svy,
 # output analysis with out tables -----------------------------------------
 
 analysis_out_list <- list("Refugee analysis" = df_analysis_refugee$results_table %>% 
+                              correct_nan_analysis_var_variable_is_na() %>% 
                               mutate(population = "refugee"),
                           "Host analysis" = df_analysis_host$results_table %>% 
+                              correct_nan_analysis_var_variable_is_na() %>% 
                               mutate(population = "host_community"))
 
 openxlsx::write.xlsx(analysis_out_list, paste0("outputs/", butteR::date_file_prefix(), "_analysis_UGA2401_echo_adjumani.xlsx"))
