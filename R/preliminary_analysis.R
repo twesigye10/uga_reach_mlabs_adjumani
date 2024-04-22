88dc84de8ef5aa7ce38158c377d5f691f0afe98f
Anthony Twesigye <twesigye10@gmail.com>
2024-04-22 09:33:42 +0300

    Revert "removing weights"
    This reverts commit 914fae06d29b2455b40d4caa88ef77f746ebc9e8.

--- R/preliminary_analysis.R
+++ R/preliminary_analysis.R
@@ -21,6 +21,10 @@ dap <- read_csv("inputs/r_dap_echo_adjumani.csv") %>%
     filter(!group_var %in% c("reason_for_health_facility_choice",
                             "hh_length_stay_adjumani_city"))
 
+# pop
+df_ref_pop <- read_csv("inputs/refugee_population_echo_adjumani.csv")
+df_host_pop <- read_csv("inputs/host_population_echo_adjumani.csv")
+
 # data with composites
 df_data_with_composites <- df_main_clean_data %>% 
     create_composite_indicators() %>% 
@@ -36,10 +40,14 @@ df_data_with_composites <- df_main_clean_data %>%
 
 # refugee analysis --------------------------------------------------------
 
-# filter refugee
-df_ref_with_composites <- df_data_with_composites %>% 
-    filter(status %in% c("refugee"))
-ref_svy <- as_survey(.data = df_ref_with_composites, strata = strata)
+# weights
+df_ref_with_weights <- analysistools::add_weights(dataset = df_data_with_composites %>% 
+                                                      filter(status %in% c("refugee")),
+                                                  sample_data = df_ref_pop,
+                                                  strata_column_dataset = "strata",
+                                                  strata_column_sample = "strata",
+                                                  population_column =  "population")
+ref_svy <- as_survey(.data = df_ref_with_weights, strata = strata, weights = weights)
 
 df_analysis_refugee <- analysistools::create_analysis(design = ref_svy, 
                                                       loa = dap,
@@ -47,11 +55,15 @@ df_analysis_refugee <- analysistools::create_analysis(design = ref_svy,
 
 # host analysis -----------------------------------------------------------
 
-# filter host
-df_host_with_composites <- df_data_with_composites %>% 
-    filter(status %in% c("host_community"))
+# weights
+df_host_with_weights <- analysistools::add_weights(dataset = df_data_with_composites %>% 
+                                                       filter(status %in% c("host_community")),
+                                                   sample_data = df_host_pop,
+                                                   strata_column_dataset = "strata",
+                                                   strata_column_sample = "strata",
+                                                   population_column =  "population")
 
-host_svy <- as_survey(.data = df_host_with_composites, strata = strata)
+host_svy <- as_survey(.data = df_host_with_weights, strata = strata, weights = weights)
 
 df_analysis_host <- analysistools::create_analysis(design = host_svy, 
                                                       loa = dap,
